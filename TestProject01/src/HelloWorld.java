dab7bd8edb56e8303f841dc5169c9ba3be3711e6
yasuhisa061 <yama061@icloud.com>
2019-10-23 17:55:03 +0900

    10/23

---
+++ TestProject01/src/HelloWorld.java
@@ -0,0 +1,24 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	
+	float a=10/3;
+	System.out.println(a);
+	
+	}
+		
+		
+		
+		  
+		  }		
+		
+		
+	
+		
+  
+  
+	
+	
+
+ 
