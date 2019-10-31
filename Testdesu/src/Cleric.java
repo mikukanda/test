26eb6c6fbf2aff6977538bd2a54d9b5fa7216967
yasuhisa061 <yama061@icloud.com>
2019-10-31 17:55:06 +0900

    10/31

---
+++ Testdesu/src/Cleric.java
@@ -0,0 +1,15 @@
+
+public class Cleric {
+String name;
+int hp=50;
+final int MAX_HP=50;
+int mp=10;
+final int MAX_MP=10;
+
+public void selfAid(){
+	System.out.println(this.name+"はセルフエイドを使った!");
+	int mp -=5
+    int hp=this.hp			
+	
+}
+}
