cb082a3c7ede81080e4a86c55c8020f10e882a38
mikukanda <mikurin7739@icloud.com>
2019-11-07 17:23:22 +0900

    11/7

--- login/sql/struts.sql
+++ login/sql/struts.sql
@@ -1,12 +1,12 @@
-set name utf8;
+set names utf8;
 set foreign_key_checks=0;
-drop database  if exists logindb kannda;
+drop database if exists logindb_kannda;
 create database logindb_kannda;
 use logindb_kannda;
 
 create table user(
 id int,
-user_name varchar(255);
+user_name varchar(255),
 password varchar(255)
 );
 
